ef024a228ae9c14e0ca5794102aa8ea523dcd7cc
PLISSON Clement <clement.plisson@telecomnancy.eu>
2025-01-06 13:53:13 +0100

    version fonctionnel

--- app/src/main/java/codenames/Main.java
+++ app/src/main/java/codenames/Main.java
@@ -1,19 +1,56 @@
 package codenames;
 
+import codenames.controller.GameController;
+import codenames.structure.CardType;
+import codenames.structure.Game;
+import codenames.structure.ListCard;
+import codenames.structure.TextCard;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) throws Exception {
-        BorderPane root = new BorderPane();
+    public void start(Stage primaryStage) throws Exception {
 
-        Scene scene = new Scene(root, 1000, 1000);
-        stage.setTitle("Codenames");
-        stage.setScene(scene);
-        stage.show();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/Game.fxml"));
+        
+        GameController controller = new GameController(testGame());
+        loader.setController(controller);
+
+        Scene scene = new Scene(loader.load());
+
+        primaryStage.setScene(scene);
+        primaryStage.setMaximized(true);
+        primaryStage.setTitle("CodeName");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private Game testGame(){
+        ListCard listCard = new ListCard();
+
+        String[] texts = {
+            "Apple", "Banana", "Cherry", "Dog", "Elephant", 
+            "Football", "Guitar", "Helicopter", "Igloo"
+        };
+
+        int i = 0;
+
+        for (String text : texts) {
+            if (i % 2 == 0) listCard.addCard(new TextCard(CardType.Blue,text));
+            else listCard.addCard(new TextCard(CardType.Red,text));
+            i++;
+        }
+
+        return new Game(3, 3, listCard);
+
+
+
     }
 }
 
