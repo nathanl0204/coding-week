0c8edb9dec4d3d74cd9577bb71867d618ddb2eee
Nathan Liénard <nathan.lienard@telecomnancy.eu>
2025-01-06 17:13:05 +0100

    Ajout de la barre de menu (sans les fonctionnalités correspondantes pour le moment)

--- src/main/java/codenames/Main.java
+++ src/main/java/codenames/Main.java
@@ -1,5 +1,6 @@
 package codenames;
 
+import codenames.view.CodenamesMenuBar;
 import codenames.view.LoadingBar;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -9,12 +10,13 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
+        CodenamesMenuBar menuBar = new CodenamesMenuBar();
         LoadingBar loadingBar = new LoadingBar(300, 30);
         loadingBar.start(30); // 30 seconds timer
 
         VBox root = new VBox(20);
         root.setStyle("-fx-padding: 20px;");
-        root.getChildren().add(loadingBar);
+        root.getChildren().addAll(menuBar, loadingBar);
 
         Scene scene = new Scene(root);
         primaryStage.setTitle("Codenames Timer");
