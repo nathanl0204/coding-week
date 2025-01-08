88be65f1eb123801e0d3d5209c7ed535054dfc67
PLISSON Clement <clement.plisson@telecomnancy.eu>
2025-01-08 10:38:46 +0100

    commit pour merge de thibault

--- app/src/main/java/codenames/Main.java
+++ app/src/main/java/codenames/Main.java
@@ -1,25 +1,17 @@
 package codenames;
 
-<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.List;
 
 import codenames.controller.GameTwoTeamsController;
-=======
-import codenames.controller.GameDuoController;
-import codenames.controller.MenuBarController;
->>>>>>> main
 import codenames.structure.CardType;
 import codenames.structure.DeckTwoTeams;
-import codenames.structure.Game;
 import codenames.structure.GameTwoTeams;
 import codenames.structure.PlayableCard;
 import codenames.structure.TextCard;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -30,7 +22,6 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-<<<<<<< HEAD
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/Game.fxml"));
         
@@ -39,15 +30,8 @@ public class Main extends Application {
         GameTwoTeamsController controller = new GameTwoTeamsController(game);
 
         loader.setController(controller);
-=======
-        Game game = testGame();
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/Game.fxml"));
-        loader.setControllerFactory(iC->new GameDuoController(game));
-        BorderPane root = loader.load();
->>>>>>> main
-
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(loader.load());
 
         primaryStage.setScene(scene);
         primaryStage.setMaximized(true);
@@ -59,19 +43,19 @@ public class Main extends Application {
         List<PlayableCard> listCard = new  ArrayList<PlayableCard>();
 
         String[] texts = {
-                "Apple", "Banana", "Cherry", "Dog", "Elephant", "Football",
-                "Guitar", "Helicopter", "Igloo", "Jacket", "Kangaroo", "Lemon",
-                "Monkey", "Notebook", "Octopus", "Penguin", "Quilt", "Rocket",
-                "Sunflower", "Tiger", "Umbrella", "Violin", "Whale", "Xylophone",
-                "Yacht"
+            "Apple", "Banana", "Cherry", "Dog", "Elephant", "Football", 
+            "Guitar", "Helicopter", "Igloo", "Jacket", "Kangaroo", "Lemon",
+            "Monkey", "Notebook", "Octopus", "Penguin", "Quilt", "Rocket",
+            "Sunflower", "Tiger", "Umbrella", "Violin", "Whale", "Xylophone",
+            "Yacht"
         };
-
-        CardType[] cardTypes = {
-                CardType.Red, CardType.Red, CardType.Blue, CardType.Black, CardType.White, CardType.Blue,
-                CardType.Blue, CardType.Red, CardType.White, CardType.Red, CardType.Blue, CardType.Black,
-                CardType.White, CardType.Red, CardType.Blue, CardType.Blue, CardType.Red, CardType.White,
-                CardType.Blue, CardType.Red, CardType.Red, CardType.White, CardType.Blue, CardType.Red,
-                CardType.Blue
+        
+        CardType[] cardTypes = { 
+            CardType.Red, CardType.Red, CardType.Blue, CardType.Black, CardType.White, CardType.Blue,
+            CardType.Blue, CardType.Red, CardType.White, CardType.Red, CardType.Blue, CardType.Black,
+            CardType.White, CardType.Red, CardType.Blue, CardType.Blue, CardType.Red, CardType.White,
+            CardType.Blue, CardType.Red, CardType.Red, CardType.White, CardType.Blue, CardType.Red,
+            CardType.Blue
         };
 
         int i = 0;
