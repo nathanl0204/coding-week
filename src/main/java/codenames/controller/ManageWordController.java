1deb22232156078c6a273199132bfcd95b35d1ae
althi <thibaud.limbach@telecomnancy.eu>
2025-01-08 10:07:08 +0100

    commit before merge

--- src/main/java/codenames/controller/ManageWordController.java
+++ src/main/java/codenames/controller/ManageWordController.java
@@ -2,13 +2,13 @@ package codenames.controller;
 
 import codenames.controller.view.InputDialogView;
 import javafx.fxml.FXML;
+import javafx.scene.SubScene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
 
@@ -17,9 +17,11 @@ public class ManageWordController {
     public ListView<String> wordsContainer;
     public ListView<String> listContainer;
     public Button removeList;
+    public Button addWordInList;
+    public Button RemoveWordInList;
     @FXML
     private Button addNewList;
-    private ArrayList<String> currentList;
+    private ArrayList<String> currentList = new ArrayList<>();
 
     public ManageWordController() {
     }
@@ -30,6 +32,9 @@ public class ManageWordController {
         removeList.setOnAction(e -> {
             deleteOneList();
         });
+        listContainer.setOnMouseClicked(e -> { loadCurrentList();
+        });
+        addWordInList.setOnAction(e -> addWordInList());
         update();
     }
 
@@ -85,10 +90,49 @@ public class ManageWordController {
 
     public void update() {
         loadList();
+        loadCurrentList();
+
+    }
+
+    public void loadCurrentList() {
+        currentList.clear();
+        if(listContainer.getSelectionModel().getSelectedItem() == null)
+            return;
+
+        String fileName = listContainer.getSelectionModel().getSelectedItem();
+        String dirPath = "resources/wordslist/";
+        String filePath = dirPath + fileName;
+        File file = new File(filePath);
+
+        if(file.exists()) {
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                try {
+                    String line = "ee";
+                    while (line != null) {
+                        line = br.readLine();
+                        currentList.add(line);
+                    }
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        DisplayCurrentList();
+    }
+
+    public void DisplayCurrentList() {
+        wordsContainer.getItems().clear();
+        for(String s : currentList) {
+            wordsContainer.getItems().add(s);
+        }
     }
 
     public void deleteOneList() {
-        System.out.println("azerty");
         if(listContainer.getSelectionModel() != null) {
             String fileName = listContainer.getSelectionModel().getSelectedItem();
             String dirPath = "resources/wordslist/";
@@ -103,4 +147,37 @@ public class ManageWordController {
 
         }
     }
+
+
+    public void addWordInList() {
+        if(listContainer.getSelectionModel() == null) {
+            new Alert(Alert.AlertType.ERROR, "Select list befor", ButtonType.OK);
+            return;
+        }
+        InputDialogView dialog = null;
+        try {
+            dialog = new InputDialogView();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String word = dialog.getValue();
+
+        currentList.add(word);
+        saveCurrentList();
+        update();
+    }
+
+    /*public void saveCurrentList() {
+        String fileName = listContainer.getSelectionModel().getSelectedItem();
+        String dirPath = "resources/wordslist/";
+        String filePath = dirPath + fileName;
+        File file = new File(filePath);
+        StringBuilder sb = new StringBuilder();
+
+        for(String s : currentList) {
+            sb.append(s).append("\n");
+        }
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+    }*/
 }
