db24484b177812106f8765a5b4f7cbffa26dd5e3
GEORGEON Gautier <gautier.georgeon@telecomnancy.eu>
2025-01-09 15:49:10 +0100

    initial commit

--- app/src/main/java/codenames/Main.java
+++ app/src/main/java/codenames/Main.java
@@ -78,7 +78,6 @@ public class Main extends Application {
         Scene scene = new Scene(root);
 
         primaryStage.setScene(scene);
-        primaryStage.setMaximized(true);
         primaryStage.setTitle("CodeName");
         primaryStage.show();
 
